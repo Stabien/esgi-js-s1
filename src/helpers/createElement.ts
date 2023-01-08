1cf1e609db0c732a4cce479f55b0161854594cde
BASTIEN PIEDALLU <bastien.piedallu@gmail.com>
2023-01-08 19:27:08 +0100

    feat(front): add renderTemplate method which render object in html

--- src/helpers/createElement.ts
+++ src/helpers/createElement.ts
@@ -1,12 +1,13 @@
-import { Attribute } from '../types'
+import { Attribute, ListenerFunction, Template } from '../types'
 
 /**
- * Creates HTML element allowing to pass an object
- * with unlimited properties to add attributes
+ * Creates HTML element allowing to pass attributes
+ * and children
  */
 export const createElement = (
   tagName: string,
   attributes: Attribute | null = null,
+  children: HTMLElement | string | null = null,
 ): HTMLElement => {
   const element: HTMLElement = document.createElement(tagName)
 
@@ -18,5 +19,66 @@ export const createElement = (
     }
   }
 
+  if (children !== null) {
+    if (typeof children === typeof HTMLElement) {
+      element.appendChild(children as HTMLElement)
+    }
+    if (typeof children === 'string') {
+      element.innerHTML = children
+    }
+  }
+
   return element
+}
+
+const hasChildren = (templateElement: Template): boolean => {
+  return Object.prototype.hasOwnProperty.call(templateElement, 'children')
+}
+
+const hasText = (templateElement: Template): boolean => {
+  return Object.prototype.hasOwnProperty.call(templateElement, 'text')
+}
+
+const addProperties = (htmlElement: HTMLElement, properties: Template): void => {
+  for (const key in properties) {
+    if (typeof properties[key] === 'function') {
+      htmlElement.addEventListener(
+        key as keyof HTMLElementEventMap,
+        properties[key] as ListenerFunction,
+      )
+    } else if (key !== 'tagName' && key !== 'children' && key !== 'text') {
+      htmlElement.setAttribute(key, properties[key] as string)
+    }
+  }
+}
+
+export const renderTemplate = (
+  template: Template[],
+  parent: HTMLElement | null = null,
+  index = 0,
+): HTMLElement => {
+  const templateLength = template.length
+  const templateElement: Template = template[index]
+
+  // Actions
+  const htmlElement: HTMLElement = document.createElement(templateElement.tagName as string)
+
+  addProperties(htmlElement, templateElement)
+
+  if (parent !== null) parent.appendChild(htmlElement)
+
+  if (hasText(templateElement)) {
+    const text = document.createTextNode(templateElement.text as string)
+    htmlElement.appendChild(text)
+  }
+
+  // Conditions
+  if (hasChildren(templateElement))
+    renderTemplate(templateElement.children as Template[], htmlElement)
+
+  if (index + 1 < templateLength) {
+    renderTemplate(template, parent, index + 1)
+  }
+
+  return htmlElement
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -5,3 +5,19 @@ export interface Attribute {
 export interface Attributes {
   [key: string]: Attribute
 }
+
+export type ListenerFunction = <K extends keyof HTMLElementEventMap>(
+  this: HTMLElement,
+  ev: HTMLElementEventMap[K],
+) => void
+
+export type TemplateProperty =
+  | string
+  | number
+  | boolean
+  | ListenerFunction
+  | { [key: string]: TemplateProperty }
+
+export interface Template {
+  [key: string]: TemplateProperty | Template[]
+}
